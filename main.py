9819698fcd21eb8af84947fa6bdd36db10bd9fb9
AsadSaveBot <amanbaevrusia@gmail.com>
2026-07-31 16:58:54 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+import os
+import yt_dlp
+from telegram import Update
+from telegram.ext import Application, MessageHandler, CommandHandler, ContextTypes, filters
+
+TOKEN = " "
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "👋 Assalomu alaykum!\nInstagram, TikTok yoki YouTub bue havolasini yuboring."
+    )
+
+async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    url = update.message.text
+
+    await update.message.reply_text("⏳ Yuklanmoqda...")
+
+    ydl_opts = {
+        "outtmpl": "video.%(ext)s",
+        "format": "mp4/best"
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            filename = ydl.prepare_filename(info)
+
+        await update.message.reply_video(video=open(filename, "rb"))
+
+        os.remove(filename)
+
+    except Exception as e:
+        await update.message.reply_text(f"❌ Xatolik: {e}")
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, download))
+
+print("Bot ishga tushdi...")
+app.run_polling()
